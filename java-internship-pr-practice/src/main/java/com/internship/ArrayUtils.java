67121676aa95ce83dc615902a5ce933828501252
Mayuri <mayurikamble2104@gmail.com>
2025-12-19 00:36:48 +0530

    Returned max ele from array with base case check

--- java-internship-pr-practice/src/main/java/com/internship/ArrayUtils.java
+++ java-internship-pr-practice/src/main/java/com/internship/ArrayUtils.java
@@ -4,6 +4,26 @@ public class ArrayUtils {
 
     // ISSUE: Return maximum element from array
     public static int findMax(int[] arr) {
-        return -1;
+        //base case..
+        if (arr==null || arr.length==0)
+        {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int max=arr[0];
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if (arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr={1,12,3,20,21};
+        System.out.println("Maximum "+findMax(arr));
     }
 }
